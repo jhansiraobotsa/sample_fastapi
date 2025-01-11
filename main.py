1a835079ea675ec818cc3ec9e6627406e3c7210e
jbotsa <jhansirao.botsa@techraq.com>
2025-01-11 15:37:27 +0530

    changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_origins=["*"],  # React app URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
